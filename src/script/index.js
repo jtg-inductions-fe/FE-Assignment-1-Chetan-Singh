4827ab0ae00241441aff0be03f6a922dd88a6a95
Chetan Singh <chetan.singh@joshtechnologygroup.com>
2025-06-14 18:05:09 +0530

    CS-A1-02: Add modal to header for small screen

--- src/script/index.js
+++ src/script/index.js
@@ -1,2 +1,10 @@
 import 'normalize.css';
 import '../styles/sass/main.scss';
+
+// Modal functionality
+const menu = document.querySelector('.icon-menu');
+const modal = document.querySelector('.header__nav');
+
+menu.addEventListener('click', () => {
+    modal.classList.toggle('active');
+});
