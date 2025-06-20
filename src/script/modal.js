3e0289672c48b979c8b9a9b0897aac1e8cfc0b5c
Chetan Singh <chetan.singh@joshtechnologygroup.com>
2025-06-20 17:28:53 +0530

    CS-A1-02: Update design for menu

--- src/script/modal.js
+++ src/script/modal.js
@@ -1,47 +1,24 @@
 const menu = document.getElementById('header-menu');
 const nav = document.getElementById('header-nav');
-const focusables = document.querySelectorAll('.header__nav .btn--link');
-
-const n = focusables.length;
-const firstEm = focusables[0];
-const lastEm = focusables[n - 1];
-
-function handleTabIndex(value) {
-    focusables.forEach((item) => (item.tabIndex = value));
-}
-
-if (!nav.classList.contains('active')) {
-    handleTabIndex('-1');
-}
+const menuIcon = document.getElementById('icon-menu');
 
 menu.addEventListener('click', () => {
-    const isActive = nav.classList.toggle('active');
+    let isMenuOpen = menuIcon.classList.contains('icon-close');
 
-    if (nav.classList.contains('active')) {
-        firstEm.focus();
-        handleTabIndex('0');
-        document.body.style.overflow = 'hidden';
+    if (isMenuOpen) {
+        nav.classList.add('hidden');
+        nav.classList.remove('active');
+
+        menuIcon.classList.add('icon-menu');
+        menuIcon.classList.remove('icon-close');
     } else {
-        handleTabIndex('-1');
-        document.body.style.overflow = 'auto';
+        nav.classList.add('active');
+        nav.classList.remove('hidden');
+
+        menuIcon.classList.add('icon-close');
+        menuIcon.classList.remove('icon-menu');
     }
 
-    menu.setAttribute('aria-expanded', isActive.toString());
-    nav.setAttribute('aria-hidden', (!isActive).toString());
-});
-
-window.addEventListener('keydown', (e) => {
-    if (e.key === 'Tab') {
-        if (e.shiftKey) {
-            if (document.activeElement === firstEm) {
-                e.preventDefault();
-                lastEm.focus();
-            }
-        } else {
-            if (document.activeElement === lastEm) {
-                e.preventDefault();
-                firstEm.focus();
-            }
-        }
-    }
+    menu.setAttribute('aria-expanded', isMenuOpen.toString());
+    nav.setAttribute('aria-hidden', (!isMenuOpen).toString());
 });
