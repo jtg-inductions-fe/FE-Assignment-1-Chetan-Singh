62dfaa5cce5172a83fb1b51074cca3d2d9bbef30
Chetan Singh <chetan.singh@joshtechnologygroup.com>
2025-06-21 13:38:06 +0530

    CS-A1-02: Modify animation and cta section

--- src/script/modal.js
+++ src/script/modal.js
@@ -2,23 +2,42 @@ const menu = document.getElementById('header-menu');
 const nav = document.getElementById('header-nav');
 const menuIcon = document.getElementById('icon-menu');
 
+function handleMenuOpen() {
+    nav.classList.add('active');
+    nav.classList.remove('hidden');
+
+    menuIcon.classList.add('icon-close');
+    menuIcon.classList.remove('icon-menu');
+}
+
+function handleMenuClose() {
+    nav.classList.add('hidden');
+    nav.classList.remove('active');
+
+    menuIcon.classList.add('icon-menu');
+    menuIcon.classList.remove('icon-close');
+}
+
+window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && menuIcon.classList.contains('icon-close')) {
+        handleMenuClose();
+
+        menu.setAttribute('aria-expanded', false);
+        nav.setAttribute('aria-hidden', true);
+
+        return;
+    }
+});
+
 menu.addEventListener('click', () => {
     const isMenuOpen = menuIcon.classList.contains('icon-close');
 
     if (isMenuOpen) {
-        nav.classList.add('hidden');
-        nav.classList.remove('active');
-
-        menuIcon.classList.add('icon-menu');
-        menuIcon.classList.remove('icon-close');
+        handleMenuClose();
     } else {
-        nav.classList.add('active');
-        nav.classList.remove('hidden');
-
-        menuIcon.classList.add('icon-close');
-        menuIcon.classList.remove('icon-menu');
+        handleMenuOpen();
     }
 
-    menu.setAttribute('aria-expanded', isMenuOpen.toString());
-    nav.setAttribute('aria-hidden', (!isMenuOpen).toString());
+    menu.setAttribute('aria-expanded', !isMenuOpen);
+    nav.setAttribute('aria-hidden', isMenuOpen);
 });
